0d56199036e8030ee67dd8da44efc9c1eda80e63
Aygul Okdirova <aygul.okdirova@check24.de>
2024-02-29 15:03:13 +0100

    recipe list and detail comps

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  image =
+    'https://t3.ftcdn.net/jpg/04/28/66/58/240_F_428665823_eVnQawJvCREXknWgvaEV8KXEQqk4Wlr1.jpg';
+  recipes: Recipe[] = [
+    new Recipe('A Test REcipe', 'This is a test', this.image),
+    new Recipe('A Test REcipe', 'This is a test', this.image),
+  ];
+  constructor() {}
 }
