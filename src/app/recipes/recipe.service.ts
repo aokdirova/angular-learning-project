82fb09c464c6ce310f15980ef380cd00d807f319
Aygul Okdirova <aygul.okdirova@check24.de>
2024-03-04 21:06:35 +0100

    added ingredients to recipe item

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
@@ -7,8 +8,14 @@ export class RecipeService {
   image =
     'https://t3.ftcdn.net/jpg/04/28/66/58/240_F_428665823_eVnQawJvCREXknWgvaEV8KXEQqk4Wlr1.jpg';
   private recipes: Recipe[] = [
-    new Recipe('A Test REcipe Number 1', 'This is a test', this.image),
-    new Recipe('A Test REcipe number 2', 'This is a test', this.image),
+    new Recipe('A Test REcipe Number 1', 'This is a test', this.image, [
+      new Ingredient('carrots', 4),
+      new Ingredient('onions', 2),
+    ]),
+    new Recipe('A Test REcipe number 2', 'This is a test', this.image, [
+      new Ingredient('noodles', 4),
+      new Ingredient('beef', 2),
+    ]),
   ];
 
   getRecipes() {
