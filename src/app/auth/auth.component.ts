73b360af96a2992461fd2770f3ceb090c98e457e
Aygul Okdirova <aygul.okdirova@check24.de>
2024-03-15 10:12:34 +0100

    auth service and signup functionality

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
 export class AuthComponent {
-    
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const { email, password } = form.value;
+    this.isLoading = true;
+    if (this.isLoginMode) {
+    } else {
+      this.authService.signUp(email, password).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.isLoading = false;
+        },
+        error: (error) => {
+          this.error = 'An error occured'
+          this.isLoading = false;
+        },
+      });
+    }
+
+    form.reset();
+  }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshtoken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDEol81V17s6LbXmmprGqOKfNBcPmPcwf8',
+      { email, password, returnSecureToken: true }
+    );
+  }
+}
